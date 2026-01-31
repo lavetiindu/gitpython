429c67eb13df1caf628a5e41fea4c7519ac02e09
indu <indu@example.com>
2026-01-31 13:06:27 +0530

    Numpy simple programs

---
+++ prpt.py
@@ -0,0 +1,8 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+  def get_age(self):
+    return self.__age
+p1 = Person("indu", 18)
+print(p1.get_age())
